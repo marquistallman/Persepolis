9c3ae00525ab3aa6ad846bfd8941e846311ad3b3
marquistallman <daviricanun@gmail.com>
2025-12-26 15:47:55 -0500

    Mensaje de ayuda

--- LMMfunction/src/main/java/com/persepolis/IA/IaApplication.java
+++ LMMfunction/src/main/java/com/persepolis/IA/IaApplication.java
@@ -1,7 +1,9 @@
 package com.persepolis.IA;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IaApplication {
@@ -10,4 +12,17 @@ public class IaApplication {
 		SpringApplication.run(IaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner init() {
+		return args -> {
+			System.out.println("--- Aplicación iniciada ---");
+			System.out.println("Puerto: 8080");
+			System.out.println("--- Rutas de Prueba (TestController) ---");
+			System.out.println("Verificar Ollama: http://localhost:8080/test/ollama");
+			System.out.println("Chat Interactivo: http://localhost:8080/test/chat?message=Hola");
+			System.out.println("Historial Chat:   http://localhost:8080/test/chat/history");
+			System.out.println("--- Rutas de Prueba (ScraperController) ---");
+			System.out.println("Scraper:          http://localhost:8080/scraper");
+		};
+	}
 }
